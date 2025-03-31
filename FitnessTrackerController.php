1af53dba8e33d6d07924606f0fc3584b8c2494f7
xuangj <xuangjin@gmail.com>
2025-03-31 09:42:19 -0400

    created createAccount and login php files. Implemented createAccount logic and some login logic in controller

--- FitnessTrackerController.php
+++ FitnessTrackerController.php
@@ -27,54 +27,103 @@ class FitnessTrackerController{
                 break;
             case "login":
                 $this->login();
+                $this->showLogin();
                 break;
             //case "dashboard":
             case "welcome":
             default:
-                $this->showWelcome();
+                $this->createAccount();
+                $this->showWelcome(); // this is being hit for some reason and not case create account
                 break;
         }
 
     }
 
     public function showWelcome($message=""){
-        include(__DIR__ . '/createAccount.html'); 
+        include(__DIR__ . '/login.php'); 
     }
 
+    
+    // Create Account logic
     public function showCreateAccount($message=""){
-        include(__DIR__ . '/createAccount.html'); 
+        include(__DIR__ . '/createAccount.php'); 
     }
-
     private function checkUserExist(){
         $query = "SELECT * FROM users WHERE email = $1;";
         $result = pg_query_params($this->db, $query, [$_POST["Email"]]);
-        echo "hit1";
         return pg_num_rows($result) > 0;
-        
     }
 
-    public function createAccount(){
+    private function checkUsernameTaken(){
+        $query = "SELECT * FROM users WHERE username = $1;";
+        $result = pg_query_params($this->db, $query, [$_POST["Username"]]);
+        return pg_num_rows($result) > 0;
+    }
 
+    public function createAccount($message =""){
         if (!isset($_POST["Name"]) || !isset($_POST["Email"]) 
             || !isset($_POST["Password"]) ||  !isset($_POST["Username"])
             || empty($_POST["Name"]) || empty($_POST["Username"])
             || empty($_POST["Email"]) || empty($_POST["Password"])){
                 $this->showCreateAccount("Please fill in all information.");
+                return;
         }
 
-        if($this->checkUserExist() === false){
-            $hashedPasswd = password_hash($_POST["Password"], PASSWORD_DEFAULT);
-            $query = "insert into users (name, username, email, password) values ($1, $2, $3, $4);";
-            $params = [$_POST["Name"], $_POST["Username"], $_POST["Email"], $hashedPasswd];
-            $createUser = pg_query_params($this->db, $query, $params);
-
-            
-            $_SESSION["name"] = $_POST["Name"];
-            $_SESSION["username"] = $_POST["Username"];
-            $_SESSION["email"] = $_POST["Email"];
-            
-            header("Location: ?command=welcome");
+        if($this->checkUserExist() === true){
+            $this->showCreateAccount("This email is linked to an existing account.  Would you like to log in?");
             return;
         }
+
+        if($this->checkUsernameTaken() === true){
+            $this->showCreateAccount("This username is already taken. Try something else!");
+            return;
+        }
+
+        $hashedPasswd = password_hash($_POST["Password"], PASSWORD_DEFAULT);
+        $query = "insert into users (name, username, email, password) values ($1, $2, $3, $4);";
+        $params = [$_POST["Name"], $_POST["Username"], $_POST["Email"], $hashedPasswd];
+        $createUser = pg_query_params($this->db, $query, $params);
+
+        
+        $_SESSION["name"] = $_POST["Name"];
+        $_SESSION["username"] = $_POST["Username"];
+        $_SESSION["email"] = $_POST["Email"];
+        
+        header("Location: ?command=welcome");
+        return;
+    }
+
+    // Login logic
+    public function showLogin($message=""){
+        include(__DIR__ . '/login.php'); 
+    }
+
+    public function login($message=""){
+        if (!isset($_POST["Email"]) || !isset($_POST["Password"]) || 
+            empty($_POST["Password"]) || empty($_POST["Email"])) {
+            $this->showWelcome("Missing information");
+            return;
+        }
+        echo "hit";
+        $email = trim($_POST["Email"]);
+        $password = $_POST["Password"];
+
+        $user = $this->retrieveUser($email);
+
+        if (empty($user)){
+            $this->showWelcome("This email is not connected to an account. Would you like to sign up?");
+        
+        } 
+        if(password_verify($password, $user["password"])){
+            $_SESSION["name"] = $user["name"];
+            $_SESSION["email"] = $user["email"];
+            $_SESSION["username"] = $user["username"];
+        }
+        else {
+            $this-> showWelcome("<p class='alert alert-danger'>Incorrect password!</p>");
+        }
+        echo "success!";
+        header("Location: ?command=createAccount");
+        exit;
     }
 }
---
+++ login.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Xuang Jin">
+        <meta name="description" content="Log in scene">
+        <meta name="keywords" content="Log in">
+
+        <meta property="og:title" content="Log In">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://cs4640.cs.virginia.edu/pnq6th/sprint2/log-in.html">
+        <meta property="og:description" content="Log in with credentials">
+        <meta property="og:site_name" content="Log In">
+
+        <title>Log-In Scene</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"  integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"  crossorigin="anonymous"> 
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+        <link rel="stylesheet" href="styles/main.css">
+
+        <!--Sources:
+        https://www.w3schools.com/css/css3_shadows.asp
+        https://www.w3schools.com/cssref/css3_pr_justify-content.php
+        -->
+    </head>
+
+    <body>
+         <!--Navigation bar includes web info link, Log In, and Join button. Collapsible depending on screen size -->
+        <nav class="navbar navbar-dark bg-dark navbar-expand-md" aria-label="Main Navigation Bar">
+            <div class="container-xl">
+                <a class="navbar-brand" href="#" style="font-size:x-large"> LOGO</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <div class="ms-auto d-flex align-items-center">
+                        <button class="btn me-auto" type="button" style="color:  white; font-size:larger"><u>About Us</u></button>
+                        <a class="btn btn-primary" href="logIn.html">Log In</a>
+                        <p class="mb-0 mx-2" style="color:white"> or </p>
+                        <a class="btn btn-primary" href="createAccount.html">Join for Free!</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <div class="py-5 mb-4">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-4 col-12"  id="LoginContainer">
+                        <div class="card shadow-sm">
+                            <div class="card-body">
+                                <h1 class="card-title text-center mb-4" style="padding:10px">Welcome Back!</h1>
+                                <form action="index.php?command=login" method="POST">
+                                    <?=$message?></br>
+                                    <!--Allows user to log into existing account using set username and password-->
+                                    <label for="Email">Email</label>
+                                    <div class="input-group mb-3">
+                                        <input type="text" class="form-control LogInInfo" id="Email" aria-describedby="emailHelp" name="Email">
+                                    </div>
+            
+                                    <label for="Password">Password</label>
+                                    <div class="input-group mb-3">
+                                        <input type="password" class="form-control LogInInfo" input id="Password" name="Password">
+                                    </div>
+                                    <button class="btn " type="button" id="forgotButton"><u>Forgot password?</u></button>
+                                    <div class="d-flex flex-column align-items-center"> 
+                                        <button class="btn btn-primary" type="button" id="LoginButton">Log In</button>
+                                        <p class="mb-0 mt-0" style="color:white">OR</p>
+                                        <!--Allows user to create new account-->
+                                        <a class="btn btn-primary" type="button" href="createAccount.php" id="CreateAccountButton">Create an Account</a>
+                                    </div> 
+                                </form>
+                            </div>
+                        </div> 
+                    </div> 
+                </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-mQ93r8dhb2uhT9LxeB1Mpr9ZmIdfuK4JbJ8bYvcj0Fow0PHeEq2zYkXf0Ehdc6Bf" 
+        crossorigin="anonymous"></script>
+    </body>
+</html>
